8fd69d4213a56ccfc282fdb46b0cded30eb1e076
hecticme <hangoc.minh1306@gmail.com>
2024-04-13 20:47:29 +0700

    chore: add glob patterns

---
+++ src/globs.ts
@@ -0,0 +1,7 @@
+export const GLOB_JS = '**/*.{js,jsx,mjs,cjs}'
+export const GLOB_TS = '**/*.{ts,tsx,mts,cts}'
+export const GLOB_VUE = '**/*.vue'
+
+export const GLOBS_EXCLUDE = [
+  '**/.nuxt',
+]
